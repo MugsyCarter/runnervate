bb62a041c420e8d6a243fbbea88e9f922f093d59
Mugsy Carter <mugsycarter@gmail.com>
2017-09-02 15:07:24 -0700

    added more stying to the home page, including a dynamically changing MBTI type

--- app/src/components/home/home.js
+++ app/src/components/home/home.js
@@ -6,4 +6,25 @@ export default {
 };
 
 
-function controller() {};
+controller.$inject = ['$timeout'];
+
+function controller(timeout) {
+
+    this.letterOptions = [['I', 'E'],['N', 'S'],['T','F'],['J','P']];
+    this.letters=[];
+
+
+    this.update = ()=>{
+        for(let i=0;i<this.letterOptions.length; i++){
+            this.letters[i] = this.letterOptions[i][Math.round(Math.random())];
+        } 
+    };
+
+    this.update();
+
+    this.stop = false;
+    while(this.stop === false){
+        timeout(()=>{this.update();}, 1000);
+    }
+
+};

1d9d469277b7f13fc057352a55c007d0c02ac390
Mugsy Carter <mugsycarter@gmail.com>
2017-10-13 17:22:12 -0700

    addTrip controller added

---
+++ app/src/components/addTrip/addTrip.js
@@ -0,0 +1,12 @@
+import template from './addTrip.html';
+
+export default {
+    template,
+    controller
+};
+
+controller.$inject = [ '$rootScope', '$state'];
+
+function controller(rootScope, $state) {
+
+};

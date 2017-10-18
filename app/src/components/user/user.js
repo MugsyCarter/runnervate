3ba1ada76f3e05e16a6d27eaf3b350225696e62b
Mugsy Carter <mugsycarter@gmail.com>
2017-10-18 15:59:53 -0700

    user info now showing up on user page

--- app/src/components/user/user.js
+++ app/src/components/user/user.js
@@ -9,15 +9,19 @@ controller.$inject = ['userService', '$timeout', '$rootScope'];
 
 function controller(userSvc, timeout, rootScope) {
 
-    // this.user = null;
-    // console.log('user is ', this.user);
-    
-    return userSvc.getById(rootScope.user.userId)
+    this.user = {
+        name: 'Fartlord'
+    };
+    console.log('user is ', this.user);
+    console.log('rootScope.user.userId is ', rootScope.user.userId);
+
+    userSvc.getById(rootScope.user.userId)
         .then((user) => {
             console.log('user is ', user[0]);
             this.user = user[0];
             console.log('user is ', this.user);
         });
+    
 
 
 };

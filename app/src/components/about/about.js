1c35a52bb3d27c9d1dffcb525a2c2c8cf43d50bd
Mugsy Carter <mugsycarter@gmail.com>
2017-11-01 17:15:20 -0700

    basic templating and database calls added for the trip browser

--- app/src/components/about/about.js
+++ app/src/components/about/about.js
@@ -6,30 +6,5 @@ export default {
     controller
 };
 
-
-controller.$inject = ['$scope', '$log', 'uiGmapGoogleMapApiProvider'];
-
-function controller($scope, $log, GoogleMapApi) {
-    angular.extend($scope, {
-        map: {center: 
-        {
-            latitude: 40.1451, 
-            longitude: -99.6680  
-        }, 
-            zoom: 4 
-        },
-        searchbox: { 
-            template:'searchbox.tpl.html', 
-            events:{
-                places_changed: function (searchBox) {}
-            }
-        },
-        options: {
-            scrollwheel: false
-        }
-    });
-    
-    GoogleMapApi.then(function(maps) {
-        maps.visualRefresh = true;
-    });
+function controller() {
 };
--- app/src/components/trips/trips.js
+++ app/src/components/trips/trips.js
@@ -8,22 +8,13 @@ export default {
 controller.$inject = ['tripService', '$timeout', '$rootScope'];
 
 function controller(tripSvc, timeout, rootScope) {
-    return tripSvc.get()
+    tripSvc.get()
     .then((trip) => {
+        this.trips = trip;
         console.log('trip is ', trip);
+        console.log(this.trips[0].name);
     });
 
-    newFunction();
 }
-function newFunction() {
-    this.myMap =() => {
-        var mapCanvas = document.getElementById('map');
-        var mapOptions = {
-            center: new google.maps.LatLng(51.5, -0.2),
-            zoom: 10
-        };
-        var map = new google.maps.Map(mapCanvas, mapOptions);
-        console.log('initializing this map', map);
-    };
-}
+
 

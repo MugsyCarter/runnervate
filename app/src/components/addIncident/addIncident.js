5204aab74172094cdb6035fe8a7e996b448e4883
Mugsy Carter <mugsycarter@gmail.com>
2017-12-12 14:11:31 -0800

    added more accurate options for the incident.  revised add incident form

--- app/src/components/addIncident/addIncident.js
+++ app/src/components/addIncident/addIncident.js
@@ -9,24 +9,67 @@ controller.$inject = [ '$rootScope', 'lynchService', '$state'];
 
 function controller(rootScope, lynchSvc, $state) {
     this.incident = {
-        name: null,
-        type: null,
-        startDate: null,
-        endDate: null,
-        startLocation: null,
-        endLocation: null,
-        description: null,
-        mileage: null,
-        people: [],
-        fun: null,
-        scenery: null,
-        difficulty: null,
-        overall: null,
-        comments: [],
-        photos: [],
-        activities: []
+        indices: {
+            cwIndex: null,
+            gdIndex: null,
+            crossRefNotesCwGd: null
+        },
+        gisCoordinates: {
+            decimal: {
+                lat: 0,
+                lon: 0
+            }
+        },
+        date: {
+            year: null,
+            month: null,
+            day: null,
+            dateNotes: null
+        },
+        location: {
+            state: 'CA',
+            place: null,
+            county: null,
+            locationNotes: null
+        },
+        lynching: {
+            crowdType: null,
+            crowdSize: null,
+            open: true,
+            authorities: null,
+            crime: null,
+            punishment: null,
+            lethalityFormula: null,
+            lethality: null,
+            otherNamesMentioned: null
+        },
+        suspects: {
+            numberSuspects: null,
+            checkSuspects: true,
+            suspectNames: [],
+            suspectRaces: [],
+            confessionOrSpeech: null, 
+            suspectNotes: null
+        },
+        vitims: [
+            {
+                victimName: [],
+                victimGender: null,
+                victimRace: null,
+                victimNotes: null
+            }
+        ],
+        sourcesAndNotes: {
+            sources: null,
+            notes: null,
+            cwGdIndexDatesNotes: null,
+            origDbIndex: null
+        }
     };
-  
+
+    this.races = ['white', 'asian', 'indian', 'black', 'latino'];
+
+
     this.addThisIncident= ()=>{
         console.log('adding this incident ', this.incident);
         lynchSvc.addIncident(this.incident)

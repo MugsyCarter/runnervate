d41d180ea60e2f30fc23a3e27744c2142ff0ea33
Mugsy Carter <mugsycarter@gmail.com>
2018-03-26 16:56:17 -0700

    dashboard controller added

---
+++ app/src/components/dash/dash.js
@@ -0,0 +1,15 @@
+import template from './incidents.html';
+
+
+export default {
+    template,
+    controller
+};
+
+controller.$inject = ['lynchService', '$timeout', '$rootScope', 'googleMapsUrl', 'NgMap', '$location', '$anchorScroll'];
+
+function controller(runSvc, timeout, rootScope, googleMapsUrl, NgMap, $location, $anchorScroll) {
+    // console.log('root scoped query is ', rootScope.query);
+    this.loading = true;
+    
+};
